5486fbcaef13394efc267a877c6e63cffff48093
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-25 22:08:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  reversed_string = String.new
+  reversed_string = Array.new
   string.split(" ").each do |word|
-    reversed_string << "#{word.reverse} "
+    reversed_string << word.reverse
   end
-  reversed_string
+  reversed_string.join(" ")
 end
